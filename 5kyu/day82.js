1845aac54efac0be533c2a0d3d089df71c5e47b7
spiano <lafleurpiano@gmail.com>
2020-10-05 21:07:43 -0500

    passing all but final test on day 82

--- 5kyu/day82.js
+++ 5kyu/day82.js
@@ -21,11 +21,14 @@
 
 function number9(n){
     let nines = 0;
-    let length = n.toString().length - 1;
-    let baseNines = (length).toString() + '0'.repeat(length - 1);
-    let additionalNines = length * parseInt((length-1).toString() + '0'.repeat(length - 2));
-    console.log(baseNines, additionalNines);
-    if (length < 7) {
+    let length;
+    let baseNines;
+    let additionalNines;
+    if (n.toString().length >= 3) {
+        length = n.toString().length - 1;
+        baseNines = (length).toString() + '0'.repeat(length - 1);
+        additionalNines = length * parseInt((length - 1).toString() + '0'.repeat(length - 2));
+        console.log(parseInt(baseNines), additionalNines);
         for (let num = baseNines; num <= n; num++) {
             if (num.toString().includes(9)) {
                 for (let digit of num.toString()) {
@@ -35,15 +38,33 @@ function number9(n){
                 }
             }
         }
+    } else {
+        for (let num = 0; num <= n; num++) {
+            if (num.toString().includes(9)) {
+                nines += 1;
+                if (num === '99') {
+                    nines += 1;
+                }
+            }
+        }
     }
 console.log(nines);
+    if (parseInt(baseNines) < additionalNines) {
+        console.log('return the additional nines');
+        return additionalNines;
+    }
     if (parseInt(baseNines) > nines) {
         console.log('The answer is ' + baseNines);
         return parseInt(baseNines);
     } else {
         let answer = nines + additionalNines;
-        console.log('Need to add ' + additionalNines + ' for an answer of ' + answer);
-        return parseInt(baseNines) + nines;
+        if (additionalNines !== undefined) {
+            console.log('Need to add ' + additionalNines + ' for an answer of ' + answer);
+            return answer;
+        } else {
+            console.log('just return the nines');
+            return nines;
+        }
     }
 }
 
@@ -56,8 +77,8 @@ console.log(nines);
 // number9(1); // => 0, 'Nein!'
 // number9(9); // => 1, 'Nein!'
 // number9(100); // => 20, 'Nein!'
-// number9(899);
 // number9(10000); // => 4000
-// number9(565754); // => 275645, 'Nein!'
+number9(565754); // => 275645, 'Nein!'
 // number9(999999);
-number9(10000000000); // => 10000000000, 'Nein!'
+// number9(10000000000); // => 10000000000, 'Nein!'
+number9(1200000000000);
